be7796c09a30fb936bd8eb3a056512b7beb56f04
VikManz <vikmanza@gmail.com>
2023-08-25 00:01:56 +0300

    review level 1

--- app/src/main/java/com/example/myapplication/AuthActivity.kt
+++ app/src/main/java/com/example/myapplication/AuthActivity.kt
@@ -1,72 +1,102 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.widget.doOnTextChanged
 import com.example.myapplication.databinding.ActivityAuthBinding
 
-const val APP_PREFERENCES = "APP_PREFERENCES"
-
 class AuthActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAuthBinding
-    private lateinit var preferences: SharedPreferences
-    private var isEmailValid = false
-    private var isPasswordValid = false
+
+
+    private val binding: ActivityAuthBinding by lazy {
+        ActivityAuthBinding.inflate(layoutInflater)
+    }
+    private val preferences: SharedPreferences by lazy {
+        getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        preferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
         loadData()
+        setListeners()
+        setFieldsValidations()
+    }
 
+    private fun loadData()
+    {
+        binding.emailEditText.setText(preferences.getString("email",""))    //todo key -> const
+        binding.passwordEditText.setText(preferences.getString("password",""))  //todo key -> const
+
+    }
+
+
+    private fun setListeners() {
         binding.registerButton.setOnClickListener { register() }
-        binding.emailEditText.doOnTextChanged { text, start, before, count ->
+    }
+
+    private fun setFieldsValidations() {
+        binding.emailEditText.doOnTextChanged { text, _, _, _ ->
             if (!isValidEmail(text.toString())) {
-                binding.emailContainer.error = "invalid email"
+                binding.emailContainer.error = "invalid email"  //todo to resources!
             } else {
                 binding.emailContainer.error = null
             }
         }
-        binding.passwordEditText.doOnTextChanged { text, start, before, count ->
-            if (text.toString().length < 8) {
-                binding.passwordContainer.error = "password must be at least 8 characters long"
+        binding.passwordEditText.doOnTextChanged { text, _, _, _ ->
+            if (text.toString().length < 8 ) {   //todo 8 -> const  && decompose pass valid
+                binding.passwordContainer.error = getString(R.string.passwordLenghtErrorText, 8)  //todo to resources!
             } else {
                 binding.passwordContainer.error = null
             }
         }
     }
 
-    private fun isValidEmail(email: String): Boolean {
-        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-        return email.matches(emailPattern.toRegex())
-    }
-    private fun loadData()
-    {
-        binding.emailEditText.setText(preferences.getString("email",""))
-        binding.passwordEditText.setText(preferences.getString("password",""))
 
-    }
+
+
     private fun register() {
-         if(binding.rememberId.isChecked) {
-             preferences.edit()
-                 .putString("password", binding.passwordEditText.text.toString())
-                 .putString("email", binding.emailEditText.text.toString())
-                 .apply()
-         }
+         if(binding.rememberId.isChecked) saveDataToSP()
          if(checkData()) {
-             val intent = Intent(this, MainActivity::class.java)
-             intent.putExtra("email", binding.emailEditText.text.toString())
+             val intent = Intent(this, MainActivity::class.java)    //todo decompose
+             intent.putExtra("email", binding.emailEditText.text.toString())    //todo key -> const
              startActivity(intent)
+             //todo finish?
              overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
          }
     }
 
+    private fun saveDataToSP() {
+        preferences.edit().putString(
+            "password",
+            binding.passwordEditText.text.toString()
+        )   //todo key -> const
+            .putString(
+                "email",
+                binding.emailEditText.text.toString()
+            ) //todo key -> const
+            .apply()
+    }
+
     private fun checkData(): Boolean {
-      return binding.passwordEditText.text.toString().length>=8 && isValidEmail(binding.emailEditText.text.toString())
+      return binding.passwordEditText.text.toString().length>=8 && isValidEmail(binding.emailEditText.text.toString())  //todo 8 -> const
+    }
+
+
+    private fun isValidEmail(email: String): Boolean {
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"   //todo pattern -> const && read about Patterns.EMAIL_ADDRESS
+        return email.matches(emailPattern.toRegex())
+    }
+
+
+    companion object {
+       private const val APP_PREFERENCES = "APP_PREFERENCES"
     }
 }
